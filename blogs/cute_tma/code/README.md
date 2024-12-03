d3b0b33ca988e6951312b78c611ee2d2b48597c8
Yifan Yang <yifayang@nvidia.com>
2024-12-02 22:41:54 -0800

    [cute_tma] official multicast code

--- blogs/cute_tma/code/README.md
+++ blogs/cute_tma/code/README.md
@@ -17,7 +17,7 @@ make
 ./cute_tma
 ```
 
-You should see result like:
+For TMA load, you should see result like:
 ```bash
 block: (0, 0), value: 0.000000, 0.000000
 block: (1, 0), value: 1.000000, 1.000000
@@ -28,4 +28,41 @@ block: (3, 0), value: 3.000000, 3.000000
 block: (2, 1), value: 3.000000, 1.000000
 block: (3, 1), value: 4.000000, 2.000000
 execution time 5.105248 ms
+```
+
+Uncomment `cute_host_multicast<c...>(...);` to use TMA multicast, you should see result like:
+```bash
+block: (4, 3, 0), cluster: (2, 1, 0), cluster rank: 2, value: 3.000000, 1.000000
+block: (5, 3, 0), cluster: (2, 1, 0), cluster rank: 3, value: 3.000000, 1.000000
+block: (4, 2, 0), cluster: (2, 1, 0), cluster rank: 0, value: 3.000000, 1.000000
+block: (5, 2, 0), cluster: (2, 1, 0), cluster rank: 1, value: 3.000000, 1.000000
+block: (6, 3, 0), cluster: (3, 1, 0), cluster rank: 2, value: 4.000000, 2.000000
+block: (6, 2, 0), cluster: (3, 1, 0), cluster rank: 0, value: 4.000000, 2.000000
+block: (3, 3, 0), cluster: (1, 1, 0), cluster rank: 3, value: 2.000000, 0.000000
+block: (2, 3, 0), cluster: (1, 1, 0), cluster rank: 2, value: 2.000000, 0.000000
+block: (3, 2, 0), cluster: (1, 1, 0), cluster rank: 1, value: 2.000000, 0.000000
+block: (7, 3, 0), cluster: (3, 1, 0), cluster rank: 3, value: 4.000000, 2.000000
+block: (7, 2, 0), cluster: (3, 1, 0), cluster rank: 1, value: 4.000000, 2.000000
+block: (2, 2, 0), cluster: (1, 1, 0), cluster rank: 0, value: 2.000000, 0.000000
+block: (1, 3, 0), cluster: (0, 1, 0), cluster rank: 3, value: 1.000000, -1.000000
+block: (0, 3, 0), cluster: (0, 1, 0), cluster rank: 2, value: 1.000000, -1.000000
+block: (1, 0, 0), cluster: (0, 0, 0), cluster rank: 1, value: 0.000000, 0.000000
+block: (0, 2, 0), cluster: (0, 1, 0), cluster rank: 0, value: 1.000000, -1.000000
+block: (1, 2, 0), cluster: (0, 1, 0), cluster rank: 1, value: 1.000000, -1.000000
+block: (0, 0, 0), cluster: (0, 0, 0), cluster rank: 0, value: 0.000000, 0.000000
+block: (3, 0, 0), cluster: (1, 0, 0), cluster rank: 1, value: 1.000000, 1.000000
+block: (2, 0, 0), cluster: (1, 0, 0), cluster rank: 0, value: 1.000000, 1.000000
+block: (3, 1, 0), cluster: (1, 0, 0), cluster rank: 3, value: 1.000000, 1.000000
+block: (1, 1, 0), cluster: (0, 0, 0), cluster rank: 3, value: 0.000000, 0.000000
+block: (2, 1, 0), cluster: (1, 0, 0), cluster rank: 2, value: 1.000000, 1.000000
+block: (0, 1, 0), cluster: (0, 0, 0), cluster rank: 2, value: 0.000000, 0.000000
+block: (4, 0, 0), cluster: (2, 0, 0), cluster rank: 0, value: 2.000000, 2.000000
+block: (5, 0, 0), cluster: (2, 0, 0), cluster rank: 1, value: 2.000000, 2.000000
+block: (4, 1, 0), cluster: (2, 0, 0), cluster rank: 2, value: 2.000000, 2.000000
+block: (5, 1, 0), cluster: (2, 0, 0), cluster rank: 3, value: 2.000000, 2.000000
+block: (7, 0, 0), cluster: (3, 0, 0), cluster rank: 1, value: 3.000000, 3.000000
+block: (6, 0, 0), cluster: (3, 0, 0), cluster rank: 0, value: 3.000000, 3.000000
+block: (7, 1, 0), cluster: (3, 0, 0), cluster rank: 3, value: 3.000000, 3.000000
+block: (6, 1, 0), cluster: (3, 0, 0), cluster rank: 2, value: 3.000000, 3.000000
+execution time 6.843968 ms
 ```
--- blogs/cute_tma/code/cute_tma.cu
+++ blogs/cute_tma/code/cute_tma.cu
@@ -1,6 +1,7 @@
 #include "cuda_runtime.h"
 #include "cuda_profiler_api.h"
 
+#include <cassert>
 #include <cstdio>
 
 #include <cutlass/cutlass.h>
@@ -73,6 +74,8 @@ __global__ void cute_tma_load_kernel(__grid_constant__ const TmaLoad tma_load, G
     // 9. after this line, the TMA load is finished
     if (threadIdx.x == 0) {
         printf("block: (%d, %d), value: %f, %f\n", blockIdx.x, blockIdx.y, float(smem_tensor(make_coord(0, 0))), float(smem_tensor(make_coord(0, 1))));
+        assert((float(smem_tensor(make_coord(0, 0))) - float(blockIdx.x + blockIdx.y)) < 1e-5);
+        assert((float(smem_tensor(make_coord(0, 1))) - float(blockIdx.x - blockIdx.y)) < 1e-5);
     }
 }
 
@@ -104,11 +107,6 @@ __global__ void cute_tma_multicast_kernel(__grid_constant__ const TmaLoad tma_lo
     using namespace cute;
     constexpr int tma_transaction_bytes = CTA_N * CTA_K * sizeof(T);
 
-    uint32_t block_rank_in_cluster = cute::block_rank_in_cluster();
-    dim3 clusterDim = cute::cluster_shape();
-    int cluster_size = clusterDim.x * clusterDim.y * clusterDim.z;
-    uint16_t tma_mcast_mask = (uint16_t(1) << cluster_size) - 1;
-
     // 1. allocate smem for the tile and memory barrier
     __shared__ T smem_data[CTA_N * CTA_K];
     __shared__ uint64_t tma_load_mbar;
@@ -123,21 +121,29 @@ __global__ void cute_tma_multicast_kernel(__grid_constant__ const TmaLoad tma_lo
         set_barrier_transaction_bytes(tma_load_mbar, tma_transaction_bytes);
     }
 
+    // 5. synchronize to make all barrier init in a cluster are done
     __syncthreads();
-    cute::cluster_sync();
+    cluster_sync();
     cutlass::arch::fence_barrier_init();
 
     if (threadIdx.x == 0) {
-        // 5. gets the coordinate of the smem tile in gmem tensor
+        // 6. gets the coordinate of the smem tile in gmem tensor
         auto gmem_tensor_coord = tma_load.get_tma_tensor(shape(gmem_tensor));
+        dim3 clusterIdx = cluster_id_in_grid();
         auto gmem_tensor_coord_cta = local_tile( // [CTA_N, CTA_K]
             gmem_tensor_coord,
             Tile<Int<CTA_N>, Int<CTA_K>>{},
-            make_coord(blockIdx.x, blockIdx.y));
+            make_coord(clusterIdx.x, clusterIdx.y));
 
-        // 6. get the slice of TMA work assigned to this CTA in a threadblock cluster 
-        auto tma_load_per_cta = tma_load.get_slice(block_rank_in_cluster);
-        // 7. issue TMA load
+        // 7. get the cluster related meta data
+        uint32_t _block_rank_in_cluster = block_rank_in_cluster();
+        dim3 clusterDim = cluster_shape();
+        int cluster_size = clusterDim.x * clusterDim.y * clusterDim.z;
+        uint16_t tma_mcast_mask = (uint16_t(1) << cluster_size) - 1;
+
+        // 8. get the slice of TMA work assigned to this CTA in a threadblock cluster 
+        auto tma_load_per_cta = tma_load.get_slice(_block_rank_in_cluster);
+        // 9. issue TMA multicast
         copy(tma_load.with(tma_load_mbar, tma_mcast_mask),
             tma_load_per_cta.partition_S(gmem_tensor_coord_cta), // [[ATOM_N, ATOM_K], CTA_N/ATOM_N, CTA_K/ATOM_K]
             tma_load_per_cta.partition_D(smem_tensor)); // [[ATOM_N, ATOM_K], CTA_N/ATOM_N, CTA_K/ATOM_K]
@@ -153,19 +159,23 @@ __global__ void cute_tma_multicast_kernel(__grid_constant__ const TmaLoad tma_lo
             printf("\n");
         }*/
     }
-    // 8. wait for TMA to finish
+    // 10. wait for TMA to finish
     __syncthreads();
     wait_barrier(tma_load_mbar, /* phase */ 0);
 
-    // 9. after this line, the TMA load is finished
+    // 11. after this line, the TMA multicast is finished
     if (threadIdx.x == 0) {
-        printf("block: (%d, %d, %d), cluster rank: %u, value: %f, %f\n", blockIdx.x, blockIdx.y, blockIdx.z, block_rank_in_cluster, float(smem_tensor(make_coord(0, 0))), float(smem_tensor(make_coord(0, 1))));
+        dim3 clusterIdx = cluster_id_in_grid();
+        uint32_t _block_rank_in_cluster = block_rank_in_cluster();
+        printf("block: (%d, %d, %d), cluster: (%d, %d, %d), cluster rank: %u, value: %f, %f\n", blockIdx.x, blockIdx.y, blockIdx.z, clusterIdx.x, clusterIdx.y, clusterIdx.z, _block_rank_in_cluster, float(smem_tensor(make_coord(0, 0))), float(smem_tensor(make_coord(0, 1))));
+        assert((float(smem_tensor(make_coord(0, 0))) - float(clusterIdx.x + clusterIdx.y)) < 1e-5);
+        assert((float(smem_tensor(make_coord(0, 1))) - float(clusterIdx.x - clusterIdx.y)) < 1e-5);
     }
 
-    cute::cluster_sync();
+    cluster_sync();
 }
 
-template <typename T, int CTA_N, int CTA_K, int CLUSTER_SIZE>
+template <typename T, int CTA_N, int CTA_K, int CLUSTER_N, int CLUSTER_K>
 void cute_host_multicast(T* data, int N, int K) {
     using namespace cute;
 
@@ -179,20 +189,20 @@ void cute_host_multicast(T* data, int N, int K) {
     auto smem_layout = make_layout(make_shape(Int<CTA_N>{}, Int<CTA_K>{}), make_stride(Int<CTA_K>{}, _1{}));
 
     // 3. create the TMA object
-    auto tma_load = make_tma_copy(SM90_TMA_LOAD_MULTICAST{}, gmem_tensor, smem_layout, Int<2>{});
+    auto tma_load = make_tma_copy(SM90_TMA_LOAD_MULTICAST{}, gmem_tensor, smem_layout, Int<CLUSTER_N * CLUSTER_K>{});
 
     // 4. invoke the kernel using extensible launch interface
     cudaLaunchConfig_t config;
     cudaLaunchAttribute attrs[1];
-    config.gridDim = dim3{N / CTA_N, K / CTA_K, CLUSTER_SIZE};
+    config.gridDim = dim3{N / CTA_N * CLUSTER_N, K / CTA_K * CLUSTER_K, 1};
     config.blockDim = 32;
     config.dynamicSmemBytes = 0;
     config.stream = 0x0;
     config.numAttrs = 1;
     attrs[0].id = cudaLaunchAttributeClusterDimension;
-    attrs[0].val.clusterDim.x = 1;
-    attrs[0].val.clusterDim.y = 1;
-    attrs[0].val.clusterDim.z = CLUSTER_SIZE;
+    attrs[0].val.clusterDim.x = CLUSTER_N;
+    attrs[0].val.clusterDim.y = CLUSTER_K;
+    attrs[0].val.clusterDim.z = 1;
     config.attrs = attrs;
 
     auto* kernel = &cute_tma_multicast_kernel<T, CTA_N, CTA_K, decltype(tma_load), decltype(gmem_tensor), decltype(smem_layout)>;
@@ -251,14 +261,14 @@ void cute_host_prefetch(T* data, int N, int K) {
 // 1. Define TMA load/prefetch tile size
 static constexpr int TILE_N = 64;
 static constexpr int TILE_K = 128;
-static constexpr int CLUSTER_SIZE = 2;
+static constexpr int CLUSTER_N = 2;
+static constexpr int CLUSTER_K = 2;
 
 int main() {
 
     int iter = 1;
 
     // 2. Define problem size and tensors
-    int M = 256;
     int N = 256;
     int K = 256;
 
@@ -285,7 +295,7 @@ int main() {
         // 4. do TMA load to smem
         cute_host_load<cutlass::float_e4m3_t, TILE_N, TILE_K>(B.device_data(), N, K);
         // uncomment this if you want to use multicast
-        //cute_host_multicast<cutlass::float_e4m3_t, TILE_N, TILE_K, CLUSTER_SIZE>(B.device_data(), N, K);
+        //cute_host_multicast<cutlass::float_e4m3_t, TILE_N, TILE_K, CLUSTER_N, CLUSTER_K>(B.device_data(), N, K);
         // uncomment this if you want to use prefetch
         //cute_host_prefetch<cutlass::float_e4m3_t, TILE_N, TILE_K>(B.device_data(), N, K);
     }
